0a490835c5def329b95673aee01bff9523bf71a2
rajalaxmi66 <ruchitha248@gmail.com>
2024-06-28 15:10:12 +0530

    modified

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import {Outlet} from 'react-router-dom'
 function Home() {
     return (
         <div className="container">
             <h1 className="mt-5">Welcome to MyApp</h1>
-            <p>This is the home page.</p>
+
+            {/* <p>This is the home page.</p> */}
+            <img src="https://th.bing.com/th/id/OIP.-vX128VI_j-j8-QOue1kugHaDt?w=346&h=175&c=7&r=0&o=5&dpr=1.3&pid=1.7" style={{"height":"350px","margin":"auto","display":"block"}}/>
         </div>
     );
 }
--- client/src/components/dashboard/Dashboard.jsx
+++ client/src/components/dashboard/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import climate from '../images/climate.jpeg'
 function Dashboard() {
   return (
     <div>
@@ -8,7 +8,7 @@ function Dashboard() {
             <div className="row">
                 <div className="col-md-4 mb-4">
                     <div className="card">
-                        <img src="https://via.placeholder.com/300x200" className="card-img-top" alt="Card Image" />
+                        <img src={climate} className="card-img-top" alt="Card Image" />
                         <div className="card-body">
                             <h5 className="card-title">Card 1</h5>
                             <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
@@ -18,7 +18,7 @@ function Dashboard() {
                 </div>
                 <div className="col-md-4 mb-4">
                     <div className="card">
-                        <img src="https://via.placeholder.com/300x200" className="card-img-top" alt="Card Image" />
+                        <img src={climate} className="card-img-top" alt="Card Image" />
                         <div className="card-body">
                             <h5 className="card-title">Card 2</h5>
                             <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
@@ -28,7 +28,7 @@ function Dashboard() {
                 </div>
                 <div className="col-md-4 mb-4">
                     <div className="card">
-                        <img src="https://via.placeholder.com/300x200" className="card-img-top" alt="Card Image" />
+                        <img src={climate} className="card-img-top" alt="Card Image" />
                         <div className="card-body">
                             <h5 className="card-title">Card 3</h5>
                             <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
@@ -41,7 +41,7 @@ function Dashboard() {
             <div className="row mt-4">
                 <div className="col-md-4 mb-4">
                     <div className="card">
-                        <img src="https://via.placeholder.com/300x200" className="card-img-top" alt="Card Image" />
+                        <img src={climate} className="card-img-top" alt="Card Image" />
                         <div className="card-body">
                             <h5 className="card-title">Card 4</h5>
                             <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
@@ -51,7 +51,7 @@ function Dashboard() {
                 </div>
                 <div className="col-md-4 mb-4">
                     <div className="card">
-                        <img src="https://via.placeholder.com/300x200" className="card-img-top" alt="Card Image" />
+                        <img src={climate} className="card-img-top" alt="Card Image" />
                         <div className="card-body">
                             <h5 className="card-title">Card 5</h5>
                             <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
@@ -61,7 +61,7 @@ function Dashboard() {
                 </div>
                 <div className="col-md-4 mb-4">
                     <div className="card">
-                        <img src="https://via.placeholder.com/300x200" className="card-img-top" alt="Card Image" />
+                        <img src={climate} className="card-img-top" alt="Card Image" />
                         <div className="card-body">
                             <h5 className="card-title">Card 6</h5>
                             <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
